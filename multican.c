189ec97d59b0006ba4395d5e88f12a13efb38e86
Christian Kelly <christian.a.kelly@gmail.com>
2022-04-15 09:55:14 +0100

    whitespace

--- multican.c
+++ multican.c
@@ -34,124 +34,127 @@ void SPI_configure() {
 }
 
 void CAN_reset() {
-	// board 0
-	gpio_put(SPI_0_CS, 0);
-	spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_RESET}, 1);
-	gpio_put(SPI_0_CS, 1);
-	busy_wait_us(100);
-	// board 1
-	gpio_put(SPI_1_CS, 0);
-	spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_RESET}, 1);
-	gpio_put(SPI_1_CS, 1);
-	busy_wait_us(100);
+    // board 0
+    gpio_put(SPI_0_CS, 0);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_RESET}, 1);
+    gpio_put(SPI_0_CS, 1);
+    busy_wait_us(100);
+    // board 1
+    gpio_put(SPI_1_CS, 0);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_RESET}, 1);
+    gpio_put(SPI_1_CS, 1);
+    busy_wait_us(100);
 }
 
 uint8_t CAN_0_reg_read(uint8_t reg) {
-	uint8_t data;
-	gpio_put(SPI_0_CS, 0);
-	spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_READ, reg}, 2);
-	spi_read_blocking(SPI_PORT, 0, &data, 1);
-	gpio_put(SPI_0_CS, 1);
-	return(data);
+    uint8_t data;
+    gpio_put(SPI_0_CS, 0);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_READ, reg}, 2);
+    spi_read_blocking(SPI_PORT, 0, &data, 1);
+    gpio_put(SPI_0_CS, 1);
+    return(data);
 }
 
 uint8_t CAN_1_reg_read(uint8_t reg) {
-	uint8_t data;
-	gpio_put(SPI_1_CS, 0);
-	spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_READ, reg}, 2);
-	spi_read_blocking(SPI_PORT, 0, &data, 1);
-	gpio_put(SPI_1_CS, 1);
-	return(data);
+    uint8_t data;
+    gpio_put(SPI_1_CS, 0);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_READ, reg}, 2);
+    spi_read_blocking(SPI_PORT, 0, &data, 1);
+    gpio_put(SPI_1_CS, 1);
+    return(data);
 }
 
 void CAN_0_reg_write(uint8_t reg, uint8_t val) {
-  gpio_put(SPI_0_CS, 0);
-  spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_WRITE, reg, val}, 3);
-  gpio_put(SPI_0_CS, 1);
+    gpio_put(SPI_0_CS, 0);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_WRITE, reg, val}, 3);
+    gpio_put(SPI_0_CS, 1);
 }
 
 void CAN_1_reg_write(uint8_t reg, uint8_t val) {
-  gpio_put(SPI_1_CS, 0);
-  spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_WRITE, reg, val}, 3);
-  gpio_put(SPI_1_CS, 1);
+    gpio_put(SPI_1_CS, 0);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_WRITE, reg, val}, 3);
+    gpio_put(SPI_1_CS, 1);
 }
 
 void CAN_0_reg_modify(uint8_t reg, uint8_t mask, uint8_t val) {
-  gpio_put(SPI_0_CS, 0);
-  busy_wait_us(2);
-  spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_MODIFY, reg, mask, val}, 4);
-  busy_wait_us(2);
-  gpio_put(SPI_0_CS, 1);
+    gpio_put(SPI_0_CS, 0);
+    busy_wait_us(2);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_MODIFY, reg, mask, val}, 4);
+    busy_wait_us(2);
+    gpio_put(SPI_0_CS, 1);
 }
 
 void CAN_1_reg_modify(uint8_t reg, uint8_t mask, uint8_t val) {
-  gpio_put(SPI_1_CS, 0);
-  busy_wait_us(2);
-  spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_MODIFY, reg, mask, val}, 4);
-  busy_wait_us(2);
-  gpio_put(SPI_1_CS, 1);
+    gpio_put(SPI_1_CS, 0);
+    busy_wait_us(2);
+    spi_write_blocking(SPI_PORT, (uint8_t[]){CMD_MODIFY, reg, mask, val}, 4);
+    busy_wait_us(2);
+    gpio_put(SPI_1_CS, 1);
 }
 
 void CAN_configure(uint16_t id) {
-  // Configure speed to 500kbps based on 8MHz Crystal
-  // Magic constants from https://github.com/sandeepmistry/arduino-CAN/blob/master/src/MCP2515.cpp
-  CAN_0_reg_write(REG_CNF1, 0x00);
-  CAN_0_reg_write(REG_CNF2, 0x90);
-  CAN_0_reg_write(REG_CNF3, 0x02);
+    // Configure speed to 500kbps based on 8MHz Crystal
+    // Magic constants from https://github.com/sandeepmistry/arduino-CAN/blob/master/src/MCP2515.cpp
+    CAN_0_reg_write(REG_CNF1, 0x00);
+    CAN_0_reg_write(REG_CNF2, 0x90);
+    CAN_0_reg_write(REG_CNF3, 0x02);
 
-  // Enable receive interrupts
-  //CAN_reg_write(REG_CANINTE, 3);
+    // Enable receive interrupts
+    //CAN_reg_write(REG_CANINTE, 3);
 
-  // Set normal operation mode
-  CAN_0_reg_write(REG_CANCTRL, MODE_NORMAL);
+    // Set normal operation mode
+    CAN_0_reg_write(REG_CANCTRL, MODE_NORMAL);
 
-  // Configure speed to 500kbps based on 8MHz Crystal
-  // Magic constants from https://github.com/sandeepmistry/arduino-CAN/blob/master/src/MCP2515.cpp
-  CAN_1_reg_write(REG_CNF1, 0x00);
-  CAN_1_reg_write(REG_CNF2, 0x90);
-  CAN_1_reg_write(REG_CNF3, 0x02);
+    // Configure speed to 500kbps based on 8MHz Crystal
+    // Magic constants from https://github.com/sandeepmistry/arduino-CAN/blob/master/src/MCP2515.cpp
+    CAN_1_reg_write(REG_CNF1, 0x00);
+    CAN_1_reg_write(REG_CNF2, 0x90);
+    CAN_1_reg_write(REG_CNF3, 0x02);
 
-  // Enable receive interrupts
-  //CAN_reg_write(REG_CANINTE, 3);
+    // Enable receive interrupts
+    //CAN_reg_write(REG_CANINTE, 3);
 
-  // Set normal operation mode
-  CAN_1_reg_write(REG_CANCTRL, MODE_NORMAL);
+    // Set normal operation mode
+    CAN_1_reg_write(REG_CANCTRL, MODE_NORMAL);
 }
 
 void CAN_0_transmit(uint16_t id, uint8_t* data, uint8_t length) {
-  CAN_0_reg_write(REG_TXBnSIDH(0), id >> 3); // Set CAN ID
-  CAN_0_reg_write(REG_TXBnSIDL(0), id << 5); // Set CAN ID
-  CAN_0_reg_write(REG_TXBnEID8(0), 0x00);    // Extended ID
-  CAN_0_reg_write(REG_TXBnEID0(0), 0x00);    // Extended ID
+    CAN_0_reg_write(REG_TXBnSIDH(0), id >> 3); // Set CAN ID
+    CAN_0_reg_write(REG_TXBnSIDL(0), id << 5); // Set CAN ID
+    CAN_0_reg_write(REG_TXBnEID8(0), 0x00);    // Extended ID
+    CAN_0_reg_write(REG_TXBnEID0(0), 0x00);    // Extended ID
 
-  CAN_0_reg_write(REG_TXBnDLC(0), length);   // Frame length
+    CAN_0_reg_write(REG_TXBnDLC(0), length);   // Frame length
 
-  for (int i = 0; i < length; i++) {       // Write the frame data
-    CAN_0_reg_write(REG_TXBnD0(0) + i, data[i]);
-  }
+    for (int i = 0; i < length; i++) {       // Write the frame data
+      CAN_0_reg_write(REG_TXBnD0(0) + i, data[i]);
+    }
 
-  CAN_0_reg_write(REG_TXBnCTRL(0), 0x08);    // Start sending
-  busy_wait_us(1000); // Allow up to 1ms to transmit
-  CAN_0_reg_write(REG_TXBnCTRL(0), 0);    // Stop sending
-  CAN_0_reg_modify(REG_CANINTF, FLAG_TXnIF(0), 0x00); // Clear interrupt flag
+    CAN_0_reg_write(REG_TXBnCTRL(0), 0x08);    // Start sending
+    busy_wait_us(1000); // Allow up to 1ms to transmit
+    CAN_0_reg_write(REG_TXBnCTRL(0), 0);    // Stop sending
+    CAN_0_reg_modify(REG_CANINTF, FLAG_TXnIF(0), 0x00); // Clear interrupt flag
 }
 
 
 void CAN_1_transmit(uint16_t id, uint8_t* data, uint8_t length) {
-  CAN_1_reg_write(REG_TXBnSIDH(0), id >> 3); // Set CAN ID
-  CAN_1_reg_write(REG_TXBnSIDL(0), id << 5); // Set CAN ID
-  CAN_1_reg_write(REG_TXBnEID8(0), 0x00);    // Extended ID
-  CAN_1_reg_write(REG_TXBnEID0(0), 0x00);    // Extended ID
+    CAN_1_reg_write(REG_TXBnSIDH(0), id >> 3); // Set CAN ID
+    CAN_1_reg_write(REG_TXBnSIDL(0), id << 5); // Set CAN ID
+    CAN_1_reg_write(REG_TXBnEID8(0), 0x00);    // Extended ID
+    CAN_1_reg_write(REG_TXBnEID0(0), 0x00);    // Extended ID
 
-  CAN_1_reg_write(REG_TXBnDLC(0), length);   // Frame length
+    CAN_1_reg_write(REG_TXBnDLC(0), length);   // Frame length
 
-  for (int i = 0; i < length; i++) {       // Write the frame data
-    CAN_1_reg_write(REG_TXBnD0(0) + i, data[i]);
-  }
+    for (int i = 0; i < length; i++) {       // Write the frame data
+      CAN_1_reg_write(REG_TXBnD0(0) + i, data[i]);
+    }
 
-  CAN_1_reg_write(REG_TXBnCTRL(0), 0x08);    // Start sending
-  busy_wait_us(1000); // Allow up to 1ms to transmit
-  CAN_1_reg_write(REG_TXBnCTRL(0), 0);    // Stop sending
-  CAN_1_reg_modify(REG_CANINTF, FLAG_TXnIF(0), 0x00); // Clear interrupt flag
+    CAN_1_reg_write(REG_TXBnCTRL(0), 0x08);    // Start sending
+    busy_wait_us(1000); // Allow up to 1ms to transmit
+    CAN_1_reg_write(REG_TXBnCTRL(0), 0);    // Stop sending
+    CAN_1_reg_modify(REG_CANINTF, FLAG_TXnIF(0), 0x00); // Clear interrupt flag
 }
 
+int main() {
+
+}
